9b5f5cc0b3c444d1ef61dcb1293ac801745b81b1
Evan Carr <ejrcarr@gmail.com>
2023-03-25 13:46:41 -0400

    Reorganized file structure

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import styles from './Navbar.module.css';
+
+const Navbar: React.FunctionComponent = () => {
+	return <nav className={styles.container}>Open Data @ Elon</nav>;
+};
+
+export default Navbar;
