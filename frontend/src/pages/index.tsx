83e70f19aba94d813d57d5bf32db105dce4cc94b
Evan Carr <ejrcarr@gmail.com>
2023-03-25 14:49:42 -0400

    Removed Head tag

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,26 +1,11 @@
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
-
-const inter = Inter({ subsets: ['latin'] });
+import styles from '@/styles/home.module.css';
 
 export default function Home() {
 	return (
-		<>
-			<Head>
-				<title>Elon ODS</title>
-				<meta
-					name='keywords'
-					content='data access, api provider, Elon University, open source, open data service, ods, elon'
-				></meta>
-				<meta
-					name='description'
-					content='Elon ODS is an Open Data Service at Elon University that provides API keys for students to access data about Elon University. Our API provider service offers data about buildings, courses, and more. Register an application today to get started!'
-				></meta>
-			</Head>
-
-			<div>
-				<h1>Welcome to Open Date Service at Elon University</h1>
-			</div>
-		</>
+		<div className={styles.container}>
+			<h1 className={styles.introTitle}>
+				Welcome to Open Date Service at Elon University
+			</h1>
+		</div>
 	);
 }
