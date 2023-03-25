943a0d18fcd95537187e968ca1cddc315a7ccdfc
Evan Carr <ejrcarr@gmail.com>
2023-03-25 13:52:51 -0400

    Removed unused image and styles

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from '../styles/page.module.css';
 import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
