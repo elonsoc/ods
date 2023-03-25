c0f3ee23f494f71579a3f7769a5afa7a982e8e4e
Evan Carr <ejrcarr@gmail.com>
2023-03-25 13:50:21 -0400

    Added footer component

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+
+const Footer: React.FunctionComponent = () => {
+	return (
+		<footer className={styles.container}>
+			Built with 🖤 by the Elon Society of Computing Project Team in Partnership
+			with the Elon University Office of IT
+		</footer>
+	);
+};
+
+export default Footer;
