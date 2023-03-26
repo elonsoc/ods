5e89858553c834546908dbc50ec52c1ef5ab652e
Evan Carr <ejrcarr@gmail.com>
2023-03-25 22:06:16 -0400

    Made org name link to home

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 const Navbar: React.FunctionComponent = () => {
-	return <nav className={styles.container}>Open Data @ Elon</nav>;
+	return (
+		<nav className={styles.container}>
+			<Link href='/'>Open Data @ Elon</Link>
+		</nav>
+	);
 };
 
 export default Navbar;
