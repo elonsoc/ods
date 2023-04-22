026226ea6ada7bb1c972a3ba813f036059ca1e36
Evan Carr <ejrcarr@gmail.com>
2023-04-22 15:52:32 -0400

    thumbs up

--- frontend/src/app/apps/[id]/page.tsx
+++ frontend/src/app/apps/[id]/page.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styles from '@/styles/application.module.css';
 
-export const ApplicationPage = () => {
-	return <div>help</div>;
+async function fetchApplication(id: any) {
+	const res = await fetch(`http://localhost:3000/api/applications?id=${id}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		cache: 'no-store',
+	});
+	const application = await res.json();
+	return application[0];
+}
+
+const ApplicationPage = async ({ params: { id } }: any) => {
+	const { name, description, owners, teamName } = await fetchApplication(id);
+	return (
+		<div className={styles.applicationContainer}>
+			<h2>{teamName}</h2>
+			<h1>{name}</h1>
+			<p>{description}</p>
+			<p>{owners}</p>
+		</div>
+	);
 };
+
+// slash login, redirect user to Elon -> login blah blah blah -> redirected to some website
+// push them to dashboard
+
+// if have token/otherwise unauthorized
 
 export default ApplicationPage;
--- frontend/src/app/apps/_components/UserApps/UserApps.tsx
+++ frontend/src/app/apps/_components/UserApps/UserApps.tsx
@@ -1,86 +1,33 @@
-import styles from './UserApps.module.css';
+'use client';
+
 import { useState } from 'react';
-import UserApp from '../UserApp/UserApp';
+import UserApp, { AppInfo } from '../UserApp/UserApp';
 import AddAppModal from '../UserAppModal/AddAppModal';
-import { Raleway } from 'next/font/google';
 import Link from 'next/link';
 
-const raleway = Raleway({ subsets: ['latin'] });
-
-interface InformationDetails {
-	title: string;
+export interface InformationDetails {
+	name: string;
 	description: string;
 	owners: string;
 	teamName: string;
 }
 
-const UserApps = () => {
+const UserApps = ({ applications, handleSubmit }: any) => {
 	const [apps, setApps] = useState<InformationDetails[]>([]);
-	const [hasApps, setHasApps] = useState<boolean>(false);
-	const [modalActive, setModalActive] = useState<boolean>(false);
+	const hasApps = applications.length;
 
-	function handleAddApp(
-		name: string,
-		description: string,
-		owners: string,
-		teamName: string
-	) {
-		const result: InformationDetails[] = [
-			...apps,
-			{
-				title: name,
-				description: description,
-				owners: owners,
-				teamName: teamName,
-			},
-		];
-		setApps(result);
-		setHasApps(true);
-		setModalActive(false);
+	async function handleAddApp(appInfo: InformationDetails) {
+		// 	// const { name, description, owners, teamName } = appInfo;
+		handleSubmit(appInfo);
 	}
 
 	return (
 		<>
-			{' '}
-			{!hasApps ? (
-				<div className={styles.noAppContainer}>
-					<header className={styles.statusContainer}>
-						<h1 className={`${raleway.className} ${styles.statusTitle}`}>
-							No Apps?
-						</h1>{' '}
-						<p className={styles.statusDescription}>
-							You currently have no registered applications.
-						</p>
-					</header>
-					<button
-						type='button'
-						onClick={() => setModalActive(true)}
-						className={styles.button}
-					>
-						Register an Application
-					</button>
-				</div>
-			) : (
-				<div className={styles.appContainer}>
-					{hasApps &&
-						apps.map((app, index) => (
-							<Link href={`apps/${index}`} key={index}>
-								<UserApp info={app} key={index} />
-							</Link>
-						))}
-
-					<button
-						type='button'
-						onClick={() => setModalActive(true)}
-						className={`${styles.button} ${styles.topRight}`}
-					>
-						Add App
-					</button>
-				</div>
-			)}
-			{modalActive && (
-				<AddAppModal onAdd={handleAddApp} onClose={setModalActive} />
-			)}
+			{applications.map((app: AppInfo) => (
+				<Link href={`apps/${app.id}`} key={app.id}>
+					<UserApp info={app} />
+				</Link>
+			))}
 		</>
 	);
 };
