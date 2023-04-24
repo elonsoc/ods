5c0c8fd5b73e699efbfce154dc9e337d938ef40b
SheedGuy <osheed420@gmail.com>
2023-04-24 19:58:46 -0400

    Prepared Statements should work!
    Added 2 prepared statements into services. I have more psql queries in applications.go than that, but they are generated base62 strings and could not possibly contain "DROP ALL TABLES".

--- backend/service/services.go
+++ backend/service/services.go
@@ -65,9 +65,23 @@ func InitStatsD(statsdURL string, log *logrus.Logger) *statsd.Client {
 }
 
 func InitDB(databaseURL string, log *logrus.Logger) *pgx.Conn {
-	connection, err := pgx.Connect(context.Background(), databaseURL)
+	ctx := context.Background()
+	connection, err := pgx.Connect(ctx, databaseURL)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Preparing a few statements that we will be using in other functions and files.
+	// Presumably this will be expanded on later and really should be organized better.
+	insertIntoKeys, err := connection.Prepare(ctx, "insert_into_keys", "INSERT INTO keys (app_ID, api_key, is_valid) VALUES ($1, $2, $3)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	insertIntoApplications, err := connection.Prepare(ctx, "insert_into_applications", "INSERT INTO applications (app_ID, app_name, description, owners, team_name) VALUES ($1, $2, $3, $4, $5)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return connection
 }
