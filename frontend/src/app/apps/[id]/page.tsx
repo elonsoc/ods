09469485808ed17ad599d06e3cb4e722af70d4e6
Evan Carr <ejrcarr@gmail.com>
2023-04-25 17:48:36 -0400

    Fixed error on trying to access invalid app id

--- frontend/src/app/apps/[id]/page.tsx
+++ frontend/src/app/apps/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '@/styles/pages/application.module.css';
 import Link from 'next/link';
+import { UserAppInformation } from '@/app/api/applications/application.d';
 
 interface ApplicationProps {
 	params: {
@@ -8,14 +9,7 @@ interface ApplicationProps {
 	};
 }
 
-interface Application {
-	name: string;
-	description: string;
-	owners: string;
-	teamName: string;
-}
-
-async function fetchApplication(id: String): Promise<Application> {
+async function fetchApplication(id: String): Promise<UserAppInformation> {
 	const res = await fetch(`http://localhost:3000/api/applications?id=${id}`, {
 		cache: 'no-cache',
 	});
@@ -24,25 +18,40 @@ async function fetchApplication(id: String): Promise<Application> {
 }
 
 const ApplicationPage = async ({ params: { id } }: ApplicationProps) => {
-	const { name, description, owners, teamName } = await fetchApplication(id);
+	const application: UserAppInformation = await fetchApplication(id);
+	if (!application) {
+		return (
+			<div className={styles.applicationContainer}>
+				<BackLink />
+				<h1>Application not found</h1>
+			</div>
+		);
+	}
+	const { name, description, owners, teamName } = application;
 	return (
 		<div className={styles.applicationContainer}>
-			<Link href='/apps' className={styles.backLink}>
-				<svg
-					className={styles.leftArrow}
-					xmlns='http://www.w3.org/2000/svg'
-					viewBox='0 0 24 24'
-				>
-					<title>Back</title>
-					<path d='M10.05 16.94V12.94H18.97L19 10.93H10.05V6.94L5.05 11.94Z' />
-				</svg>
-				Back to Apps
-			</Link>
+			<BackLink />
 			<h2>{teamName}</h2>
 			<h1>{name}</h1>
 			<p>{description}</p>
 			<p>{owners}</p>
 		</div>
+	);
+};
+
+const BackLink = () => {
+	return (
+		<Link href='/apps' className={styles.backLink}>
+			<svg
+				className={styles.leftArrow}
+				xmlns='http://www.w3.org/2000/svg'
+				viewBox='0 0 24 24'
+			>
+				<title>Back</title>
+				<path d='M10.05 16.94V12.94H18.97L19 10.93H10.05V6.94L5.05 11.94Z' />
+			</svg>
+			Back to Apps
+		</Link>
 	);
 };
 
