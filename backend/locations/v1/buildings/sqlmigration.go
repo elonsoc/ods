595c47f0108cd94cfcc180c9408c95a59dcfbb15
ronydahdal <123394337+ronydahdal@users.noreply.github.com>
2023-05-02 12:58:44 -0400

    Update sqlmigration.go

--- backend/locations/v1/buildings/sqlmigration.go
+++ backend/locations/v1/buildings/sqlmigration.go
@@ -9,6 +9,23 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type Building struct {
+	ID			string
+	Desc			sql.NullString
+	Location		sql.NullString
+	LocationRepresentation	sql.NullString
+	Type			sql.NullString
+	TypeRepresentation	sql.NullString
+	LongDesc		sql.NullString
+	City			sql.NullString
+	State			sql.NullString
+	Zip			sql.NullString
+	Sector			sql.NullString
+	Latitude		sql.NullFloat64
+	Longitude		sql.NullFloat64
+	AddDate			sql.NullTime
+	ChangeDate		sql.NullTime
+}
 func migrate() {
 	// connecting to MSSQL Server Database (Elon's in the future)
 	// MSSQL connection string w/ server address, user, pass, database name
@@ -38,6 +55,10 @@ func migrate() {
 
 	// define connection string for PSQL database
 	// connecting to a localhost psql database here
+	// postgress (prior to :odspass...) is the username
+	// odspass12345 is the placeholder pass
+	// postgres (prior to ?sslmode..) is placeholder database name
+	
 	pgConnString := "postgres://postgres:odspass12345@localhost/postgres?sslmode=disable"
 	// connecting to PostgreSQL server using Go driver from package
 	pgConn, err := sql.Open("postgres", pgConnString)
@@ -47,75 +68,33 @@ func migrate() {
 	}
 	defer pgConn.Close()
 
-	// create PSQL database w/ same schema as database
-	// empty _ because Exec returns both a sql result obj & err value
-	// we're evaluating err value, hence the _
-	_, err = pgConn.Exec(`
-    CREATE TABLE elonbuildingspg (
-        buildings_id varchar(4) NOT NULL,
-        bldg_desc varchar(255) NULL,
-        bldg_location varchar(5) NULL,
-        bldg_location_representation varchar(30) NULL,
-        bldg_type varchar(10) NULL,
-        bldg_type_representation varchar(32) NULL,
-        bldg_long_desc varchar(1996) NULL,
-        bldg_city varchar(25) NULL,
-        bldg_state varchar(2) NULL,
-        bldg_zip varchar(10) NULL,
-        bldg_sector varchar(10) NULL,
-        bldg_latitude numeric NULL,
-        bldg_longitude numeric NULL,
-        buildings_add_date timestamp NULL,
-        buildings_chgdate timestamp NULL,
-        PRIMARY KEY (buildings_id)
-    );
-`)
-
-	// if error, logs error message and termianates
-	if err != nil {
-		log.Fatal("Error creating PostgreSQL table: ", err.Error())
-	}
-
 	// inserting data into PSQL database
 	// for loop to iterate over rows of MSSQL
 
 	for mssqlRows.Next() {
-		// declaration of variables to hold values of each column in row
-		var buildings_id string
-		var bldg_desc sql.NullString
-		var bldg_location sql.NullString
-		var bldg_location_representation sql.NullString
-		var bldg_type sql.NullString
-		var bldg_type_representation sql.NullString
-		var bldg_long_desc sql.NullString
-		var bldg_city sql.NullString
-		var bldg_state sql.NullString
-		var bldg_zip sql.NullString
-		var bldg_sector sql.NullString
-		var bldg_latitude sql.NullFloat64
-		var bldg_longitude sql.NullFloat64
-		var buildings_add_date sql.NullTime
-		var buildings_chgdate sql.NullTime
+		// re-use building struct 
+		var building Building
 		// scan method retrieves values from MSSQL rows & assings to Go's memory
-		err = mssqlRows.Scan(&buildings_id, &bldg_desc, &bldg_location,
-			&bldg_location_representation, &bldg_type,
-			&bldg_type_representation, &bldg_long_desc, &bldg_city,
-			&bldg_state, &bldg_zip, &bldg_sector, &bldg_latitude,
-			&bldg_longitude, &buildings_add_date, &buildings_chgdate)
+		err = mssqlRows.Scan(&buildings.ID, &building.Desc, &building.Location,
+			&building.LocationRepresentation, &building.Type,
+			&building.TypeRepresentation, &building.LongDesc, &building.City,
+			&building.State, &building.Zip, &building.Sector, &building.Latitude,
+			&building.Longitude, &building.AddDate, &building.ChangeDate)
 		// check for errors, terminates if error
 		if err != nil {
 			log.Fatal("Error scanning row: ", err.Error())
 		}
-		// inserts values as arguments into PSQL
+		
 		// inserting values as arguments into PSQL
+		// note: elonbuildingspg is placeholder/assumed postgresql database tablename
 		_, err = pgConn.Exec(`
 	INSERT INTO elonbuildingspg (buildings_id, bldg_desc, bldg_location, bldg_location_representation, 
 		bldg_type, bldg_type_representation, bldg_long_desc, bldg_city, bldg_state, bldg_zip, 
 		bldg_sector, bldg_latitude, bldg_longitude, buildings_add_date, buildings_chgdate)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`,
-			buildings_id, bldg_desc, bldg_location, bldg_location_representation, bldg_type,
-			bldg_type_representation, bldg_long_desc, bldg_city, bldg_state, bldg_zip, bldg_sector,
-			bldg_latitude, bldg_longitude, buildings_add_date, buildings_chgdate)
+			building.ID, building.Desc, building.Location, building.LocationRepresentation, building.Type,
+			building.TypeRepresentation, building.LongDesc, building.City, building.State, building.Zip, building.Sector,
+			building.Latitude, building.Longitude, building.AddDate, building.ChangeDate)
 		if err != nil {
 			log.Fatal("Error inserting into PostgreSQL table: ", err.Error())
 		}
