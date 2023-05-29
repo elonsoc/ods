644e0afca2f6bde677c95e7da51fe32b9a6be8ed
Jumar Martin <jmartin57@elon.edu>
2023-05-29 13:33:09 -0400

    Fix main : )

--- backend/mocks/DbIFace.go
+++ backend/mocks/DbIFace.go
@@ -52,13 +52,13 @@ func (_m *DbIFace) GetConn() *pgx.Conn {
 	return r0
 }
 
-// NewApp provides a mock function with given fields: _a0, _a1, _a2, _a3, _a4, _a5, _a6
-func (_m *DbIFace) NewApp(_a0 string, _a1 string, _a2 string, _a3 string, _a4 string, _a5 string, _a6 bool) error {
-	ret := _m.Called(_a0, _a1, _a2, _a3, _a4, _a5, _a6)
+// NewApp provides a mock function with given fields: _a0, _a1, _a2, _a3, _a4, _a5
+func (_m *DbIFace) NewApp(_a0 string, _a1 string, _a2 string, _a3 string, _a4 string, _a5 bool) error {
+	ret := _m.Called(_a0, _a1, _a2, _a3, _a4, _a5)
 
 	var r0 error
-	if rf, ok := ret.Get(0).(func(string, string, string, string, string, string, bool) error); ok {
-		r0 = rf(_a0, _a1, _a2, _a3, _a4, _a5, _a6)
+	if rf, ok := ret.Get(0).(func(string, string, string, string, string, bool) error); ok {
+		r0 = rf(_a0, _a1, _a2, _a3, _a4, _a5)
 	} else {
 		r0 = ret.Error(0)
 	}
