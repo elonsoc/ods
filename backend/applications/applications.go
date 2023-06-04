319f4c9db5c19a19a3d619aeb318a470d305aeb9
Evan Carr <ejrcarr@gmail.com>
2023-06-04 09:46:58 -0400

    Added options method

--- backend/applications/applications.go
+++ backend/applications/applications.go
@@ -40,7 +40,7 @@ func NewApplicationsRouter(a *ApplicationsRouter) *ApplicationsRouter {
 	cors := cors.New(cors.Options{
 		AllowedOrigins:     []string{"*"},
 		AllowOriginFunc:    func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:     []string{"Link"},
 		AllowCredentials:   true,
