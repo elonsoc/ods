23d78202f714025d0d44397cd7811ca10ceb2be1
Jumar Martin <jmartin57@elon.edu>
2023-06-07 23:27:23 -0400

    first few steps to cleaning up everything!

--- backend/main.go
+++ backend/main.go
@@ -116,7 +116,7 @@ func CustomLogger(log service.LoggerIFace, stat service.StatIFace) func(next htt
 // At the beginning, we create a new instance of the router, declare usage of multiple middlewares
 // initialize connections to external services, and mount the various routers for the apis that we
 // will be serving.
-func initialize(servicePort, databaseURL, redisURL, loggingURL, statsdURL, certPath, keyPath, idpURL string) chi.Router {
+func initialize(servicePort, databaseURL, redisURL, loggingURL, statsdURL, certPath, keyPath, idpURL, spURL string) chi.Router {
 	startInitialization := time.Now()
 	// This is where we initialize the various services that we will be using
 	// like the database, logger, stats, etc.
@@ -126,7 +126,7 @@ func initialize(servicePort, databaseURL, redisURL, loggingURL, statsdURL, certP
 	// This particular technique is called dependency injection, and it's a good practice to use
 	// when writing code that could one day be decoupled into separate services.
 	// There are better ways to do this, but this is a good start to keep the app monolithic for now.
-	svc := service.NewService(loggingURL, databaseURL, statsdURL, certPath, keyPath, idpURL)
+	svc := service.NewService(loggingURL, databaseURL, statsdURL, certPath, keyPath, idpURL, spURL)
 	samlMiddleware := svc.Saml.GetSamlMiddleware()
 
 	// Create a new instance of the router
@@ -268,6 +268,7 @@ func main() {
 	samlCertPath := flag.String("saml_cert_path", os.Getenv("SAML_CERT_PATH"), "location of service cert")
 	samlKeyPath := flag.String("saml_key_path", os.Getenv("SAML_KEY_PATH"), "location of service key")
 	idpURL := flag.String("idp_url", os.Getenv("IDP_URL"), "url of identity provider")
+	spURL := flag.String("sp_url", os.Getenv("SP_URL"), "url of the hosted service provider")
 
 	// this could use some improvement in nameing and probably would require
 	// Hashicorp Vault or someting of the sort
@@ -295,13 +296,15 @@ func main() {
 	if *samlKeyPath == "" {
 		log.Fatal("service key location not set")
 	}
-
 	if *idpURL == "" {
 		log.Fatal("idp url not set")
 	}
+	if *spURL == "" {
+		log.Fatal("sp url not set")
+	}
 
 	err := http.ListenAndServe(fmt.Sprintf(":%s", *servicePort),
-		initialize(*servicePort, *databaseURL, *redisURL, *loggingURL, *statsdURL, *samlCertPath, *samlKeyPath, *idpURL))
+		initialize(*servicePort, *databaseURL, *redisURL, *loggingURL, *statsdURL, *samlCertPath, *samlKeyPath, *idpURL, *spURL))
 	if err != nil {
 		fmt.Println(err)
 	}
