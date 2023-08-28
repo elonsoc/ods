b2e7f834edc9c86684d3a4c7ef3d2ec02bc5e09b
Jumar Martin <git@jum.ar>
2023-08-28 17:26:10 -0400

    remove stupid debug

--- backend/main.go
+++ backend/main.go
@@ -214,9 +214,6 @@ func initialize(servicePort, databaseURL, redisURL, loggingURL, statsdURL, certP
 				surname := samlsp.AttributeFromContext(r.Context(), "urn:oid:2.5.4.4")
 				email := samlsp.AttributeFromContext(r.Context(), "urn:oid:0.9.2342.19200300.100.1.3")
 				affiliation := samlsp.AttributeFromContext(r.Context(), "urn:oid:1.3.6.1.4.1.5923.1.1.1.5")
-				sesh := samlsp.SessionFromContext(r.Context())
-				a := sesh.(samlsp.SessionWithAttributes)
-				svc.Log.Debug(fmt.Sprintf("%+v", a.GetAttributes()), nil)
 				err := svc.Db.NewUser(elon_uid, givenName, surname, email, affiliation)
 				if err != nil {
 					svc.Log.Error(err.Error(), nil)
