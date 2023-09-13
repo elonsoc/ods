a3a07288416a20a91fc99395bd58dfc5501aefbf
Jumar Martin <jmartin57@elon.edu>
2023-09-13 09:31:41 -0700

    Add Applications Test for Backend and Comment for Frontend (#48)
    * add first applications test and comment.
    
    * add python for MSSQL to PSQL migration
    
    * clean up README.md

--- frontend/src/config/Constants.tsx
+++ frontend/src/config/Constants.tsx
@@ -1,3 +1,12 @@
+/* this is a bit confusing—API_URL and BACKEND_API_URL doesn't make much sense.
+
+API_URL is just the URL that the service exists. With nextjs, our frontend calls
+<url>/api/* for api calls in the backend. The "backend api url" is used exclusively on the frontend's backend
+and, honestly, we can remove it entirely from the frontend by making it an api call.
+
+The reason I'm a bit confused is because we can call the frontend's api using just /api/* and not require
+the full api_url.
+*/
 interface Config {
 	url: {
 		API_URL: string;
