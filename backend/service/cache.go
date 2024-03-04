4952dc81d8f9d3d710895d586b7bd2018e7b8577
Evan Carr <ejrcarr@gmail.com>
2024-03-04 12:54:57 -0500

    Renamed redis-related naming to be more abstract

---
+++ backend/service/cache.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type InMemoryDbIFace interface {
+	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error
+	Get(ctx context.Context, key string) (string, error)
+	Del(ctx context.Context, key string) error
+}
+
+type InMemoryDbService struct {
+	client *redis.Client
+}
+
+func initInMemoryDb(redisURL string, log LoggerIFace) InMemoryDbIFace {
+    opts, err := redis.ParseURL(redisURL)
+    if err != nil {
+        log.Fatal(err)
+        return nil
+    }
+
+    client := redis.NewClient(opts)
+
+    _, err = client.Ping(context.Background()).Result()
+    if err != nil {
+        log.Fatal(err)
+        return nil
+    }
+
+    return &InMemoryDbService{client: client}
+}
+
+func (r *InMemoryDbService) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	return r.client.Set(ctx, key, value, expiration).Err()
+}
+
+func (r *InMemoryDbService) Get(ctx context.Context, key string) (string, error) {
+	return r.client.Get(ctx, key).Result()
+}
+
+func (r *InMemoryDbService) Del(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
+}
