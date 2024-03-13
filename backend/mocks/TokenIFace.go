23d2b3841f2379f7363e2e286fe91aef0e358f20
Evan Carr <ejrcarr@gmail.com>
2024-03-13 15:23:29 -0400

    Update mock interfaces

--- backend/mocks/TokenIFace.go
+++ backend/mocks/TokenIFace.go
@@ -111,6 +111,24 @@ func (_m *TokenIFace) InvalidateToken(_a0 string) error {
 	return r0
 }
 
+// InvalidateTokensForUid provides a mock function with given fields: _a0
+func (_m *TokenIFace) InvalidateTokensForUid(_a0 string) error {
+	ret := _m.Called(_a0)
+
+	if len(ret) == 0 {
+		panic("no return value specified for InvalidateTokensForUid")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string) error); ok {
+		r0 = rf(_a0)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // NewToken provides a mock function with given fields: _a0
 func (_m *TokenIFace) NewToken(_a0 string) (string, error) {
 	ret := _m.Called(_a0)
